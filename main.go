a05bf7c53ba4b02ba3ed39db3ede0fd25356cc2a
Filat Naumov <fnaumov@wakeapp.ru>
2019-11-08 16:22:59 +0300

    Updated main.go

--- main.go
+++ main.go
@@ -25,8 +25,8 @@ import (
 func main() {
 	var (
 		logger = log.NewLogfmtLogger(os.Stderr)
-		httpAddr = "127.0.0.1:8080"
-		grpcAddr = "127.0.0.1:8081"
+		httpAddr = ":8080"
+		grpcAddr = ":8081"
 		consulAddr = "127.0.0.1:8500"
 	)
 
